96558bca9ee91a86a94c949fe4e5c4894a42dca7
iFamished <me@ifamished.com>
2026-06-27 22:57:08 +0000

    Fix getPackVersion, redo

--- src/utils/getPackVersion.js
+++ src/utils/getPackVersion.js
@@ -1,13 +1,10 @@
 export function getPackVersion(raw) {
   if (raw.endsWith("-legacy")) return "Legacy"
 
-  const base = raw.split("-")[0]
-  const parts = base.split(".")
+  const base = raw.split("-")[0] // strip -alpha, -beta, etc.
+  const [major, minor] = base.split(".")
 
-  // 1-part or 2-part version
-  if (parts.length === 1) return `v${parts[0]}`
-  if (parts.length === 2 && parts[1] === "0") return `v${parts[0]}`
-
-  // 3-part version always shown
-  return `v${parts.join(".")}`
+  // collapse patch versions
+  if (!minor) return `v${major}`
+  return `v${major}.${minor}`
 }
