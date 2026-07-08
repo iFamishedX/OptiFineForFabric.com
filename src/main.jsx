44e16e5ad776c7b7deb26c5ed39acfacd0391b30
iFamished <me@ifamished.com>
2026-07-08 01:03:47 +0000

    Plasma

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/premium/nebula.css"
+import "ifamished-ui/styles/themes/premium/plasma.css"
 // Dream
 
 // Site-specific styles
